93e6eb5ad8e471748f7c2d26387072455503a0cc
StanislavGM <126458974+StanislavGM@users.noreply.github.com>
2023-03-01 23:36:31 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ variable vpc_security_group_ids {
 }
 variable identity {}
 variable web_count {}
-
+variable instance_type {}
 resource "aws_instance" "web" {
   ami = var.ami
-  instance_type = 
+  instance_type = var.instance_type
   subnet_id = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   count = var.web_count
