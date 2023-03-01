b7294dbfa45ac4638ee0e2131f7621b484637697
StanislavGM <126458974+StanislavGM@users.noreply.github.com>
2023-03-01 22:08:32 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable identity {}
 variable web_count {}
 resource "aws_instance" "web" {
   ami = var.ami
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   subnet_id = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   count = var.web_count
